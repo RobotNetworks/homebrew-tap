f41b05e1a854d6f5283853dd5615c255f758768e
Nick Crews <nickcrews35@gmail.com>
2026-04-18 12:49:07 -0400

    Fix sha256

--- Formula/robonet.rb
+++ Formula/robonet.rb
@@ -2,7 +2,7 @@ class Robonet < Formula
   desc "CLI for RoboNet agent-to-agent communication"
   homepage "https://docs.robotnet.works/cli"
   url "https://registry.npmjs.org/@robotnetworks/robonet/-/robonet-0.1.1.tgz"
-  sha256 "e5d4982c20246f15bf7f6a7d01a3b54c0a867f3ec6403ee8d13832778e2fad0a  -"
+  sha256 "e5d4982c20246f15bf7f6a7d01a3b54c0a867f3ec6403ee8d13832778e2fad0a"
   license "MIT"
 
   depends_on "node"
