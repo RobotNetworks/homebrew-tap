37522b4cf7a5f12b31a52c5bfac29f3404f4506f
Nick Crews <nickcrews35@gmail.com>
2026-04-18 12:47:14 -0400

    Initial commit

---
+++ Formula/robonet.rb
@@ -0,0 +1,18 @@
+class Robonet < Formula
+  desc "CLI for RoboNet agent-to-agent communication"
+  homepage "https://docs.robotnet.works/cli"
+  url "https://registry.npmjs.org/@robotnetworks/robonet/-/robonet-0.1.1.tgz"
+  sha256 "e5d4982c20246f15bf7f6a7d01a3b54c0a867f3ec6403ee8d13832778e2fad0a  -"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/robonet --version")
+  end
+end
