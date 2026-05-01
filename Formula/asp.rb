aae8500d15cbd9f190c112bdf3a2939d0adb57fd
Nick Crews <nickcrews35@gmail.com>
2026-05-01 00:00:22 -0400

    asp 0.1.0

--- Formula/asp.rb
+++ Formula/asp.rb
@@ -1,8 +1,8 @@
 class Asp < Formula
   desc "Run and inspect local Agent Session Protocol networks"
   homepage "https://github.com/RobotNetworks/asp/tree/main/cli"
-  url "https://registry.npmjs.org/@robotnetworks/asp/-/asp-0.0.0.tgz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  url "https://registry.npmjs.org/@robotnetworks/asp/-/asp-0.1.0.tgz"
+  sha256 "b62dcde027c6f3f252654a48aacdbb6edc036b430d7e19d4d0257de284e31a2b"
   license "Apache-2.0"
 
   depends_on "node"
